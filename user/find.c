51996a4cac017b4f829cb46680229909a2d98afa
shinka <1762977362@qq.com>
2024-07-17 13:52:59 +0800

    change find

--- user/find.c
+++ user/find.c
@@ -3,73 +3,80 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-int match(char *path, char *name)
+#define BUF_SIZE 512
+#define NULL 0
+
+// 实现 strrchr 的功能
+char *my_strrchr(const char *s, int c)
 {
-    char *p;
-
-    // 查找最后一个'/'后的第一个字符
-    for (p = path + strlen(path); p >= path && *p != '/'; p--)
-        ;
-    p++;
-
-    if (strcmp(p, name) == 0)
-        return 1;
-    else
-        return 0;
+    char *last = NULL;
+    while (*s)
+    {
+        if (*s == c)
+            last = (char *)s;
+        s++;
+    }
+    return last;
 }
 
-void find(char *path, char *name)
+int match(const char *path, const char *name)
 {
-    char buf[512], *p;
+    const char *p = my_strrchr(path, '/');
+    p = (p == NULL) ? path : p + 1;
+    return strcmp(p, name) == 0;
+}
+
+void find(const char *path, const char *name)
+{
+    char buf[BUF_SIZE], *p;
     int fd;
     struct dirent de;
     struct stat st;
 
     if ((fd = open(path, 0)) < 0)
     {
-        fprintf(2, "ls: cannot open %s\n", path);
+        fprintf(2, "find: cannot open %s\n", path);
         return;
     }
 
     if (fstat(fd, &st) < 0)
     {
-        fprintf(2, "ls: cannot stat %s\n", path);
+        fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
         return;
     }
 
-    switch (st.type)
+    if (st.type == T_FILE)
     {
-    case T_FILE:
         if (match(path, name))
         {
             printf("%s\n", path);
         }
-        break;
+        close(fd);
+        return;
+    }
 
-    case T_DIR:
-        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    if (st.type == T_DIR)
+    {
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf))
         {
-            printf("ls: path too long\n");
-            break;
+            printf("find: path too long\n");
+            close(fd);
+            return;
         }
         strcpy(buf, path);
         p = buf + strlen(buf);
-        // 给path后面添加 /
         *p++ = '/';
+
         while (read(fd, &de, sizeof(de)) == sizeof(de))
         {
-            // 如果读取成功, 一直都会在while loop中
-            if (de.inum == 0)
+            if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
                 continue;
-            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
-                continue;
-            // 把 de.name 拷贝到p中
+
             memmove(p, de.name, DIRSIZ);
             p[DIRSIZ] = 0;
             find(buf, name);
         }
-        break;
     }
     close(fd);
 }
@@ -78,9 +85,10 @@ int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
-        printf("argc is %d and it is less then 2\n", argc);
+        fprintf(2, "usage: find <path> <name>\n");
         exit(1);
     }
     find(argv[1], argv[2]);
     exit(0);
 }
+
